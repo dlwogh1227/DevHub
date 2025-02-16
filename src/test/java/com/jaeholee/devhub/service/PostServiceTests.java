d06690198c4c1223a6150b38c77bc504d8e73750
dlwogh1227 <dlwogh1227@naver.com>
2025-02-16 19:29:07 +0900

    docker start_2

--- src/test/java/com/jaeholee/devhub/service/PostServiceTests.java
+++ src/test/java/com/jaeholee/devhub/service/PostServiceTests.java
@@ -1,16 +1,11 @@
 package com.jaeholee.devhub.service;
 
 import com.jaeholee.devhub.DevHubApplication;
-import com.jaeholee.devhub.domain.Attachment;
-import com.jaeholee.devhub.domain.AttachmentType;
-import com.jaeholee.devhub.domain.Post;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-
-import java.io.File;
 
 @SpringBootTest
 @ContextConfiguration(classes = DevHubApplication.class)
